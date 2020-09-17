d20dd58565b4ffbe59d0e6aece487cc7e662f5db
Yuto Sone <sny0421@Moonstone.local>
2020-09-17 22:46:48 +0900

    update script

--- Windows_10_EVD_2004/scripts/04_config_system_language.ps1
+++ Windows_10_EVD_2004/scripts/04_config_system_language.ps1
@@ -32,7 +32,3 @@ REG UNLOAD $DefaultHKEY
 REG IMPORT $welcomePath
 
 gpupdate /force
-# Update UWP Apps
-Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod
-
-Start-Sleep -s 900
